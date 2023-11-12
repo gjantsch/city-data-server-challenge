ec2c96b8c0a0a36172a3668782b410381db8906e
Gustavo Jantsch <jantsch@gmail.com>
2023-11-12 16:05:26 -0300

    Final version

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Form;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Get a list of forms
+Route::get("/forms", function (Request $request) {
+
+    return response()->json(Form::all()->toArray());
+
+});
+
+// Handles PUT/PATCH Form requests
+Route::post('/form', function (Request $request) {
+
+    $data = Form::find($request->input('id', 0));
+
+    if (!$data) {
+        $data = new Form();
+    }
+
+    $updates = ['status' => Form::STATUS_WAITING];
+
+    if ($request->input('name')) {
+        $updates['name'] = $request->input('name');
+    }
+
+    if ($request->input('amount')) {
+        $updates['amount'] = $request->input('amount');
+    }
+
+    if ($request->input('status')) {
+        $updates['status'] = $request->input('status');
+    }
+
+    $data->fill($updates);
+
+    $data->save();
+
+    return response()->json($data);
+
+});
+
+// Get a single form from its ID
+Route::get("/form/{id}", function (Request $request, $id) {
+    return response()->json(Form::find($id)->toArray());
 });
